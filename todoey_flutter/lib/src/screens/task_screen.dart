889b89f99b44c56aca9f2f2c82fcff451d3f0071
cromewar <vasiliy@cromewar.com>
2021-03-09 09:54:28 -0500

    end of task app

--- todoey_flutter/lib/src/screens/task_screen.dart
+++ todoey_flutter/lib/src/screens/task_screen.dart
@@ -8,100 +8,98 @@ class TaskScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        backgroundColor: kBacgroundColor,
-        floatingActionButton: Container(
-          width: 60.0,
-          height: 60.0,
-          child: FloatingActionButton(
-            backgroundColor: kBacgroundColor,
-            onPressed: () {
-              showModalBottomSheet(
-                context: context,
-                builder: (BuildContext context) => AddTaskScreen(),
-                isScrollControlled: true,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(30.0),
-                    topRight: Radius.circular(30.0),
-                  ),
+    return Scaffold(
+      backgroundColor: kBacgroundColor,
+      floatingActionButton: Container(
+        width: 60.0,
+        height: 60.0,
+        child: FloatingActionButton(
+          backgroundColor: kBacgroundColor,
+          onPressed: () {
+            showModalBottomSheet(
+              context: context,
+              builder: (BuildContext context) => AddTaskScreen(),
+              isScrollControlled: true,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30.0),
+                  topRight: Radius.circular(30.0),
                 ),
-              );
-            },
-            child: Icon(
-              Icons.add,
-              size: 60.0,
-            ),
+              ),
+            );
+          },
+          child: Icon(
+            Icons.add,
+            size: 60.0,
           ),
         ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              padding: EdgeInsets.only(
-                  top: 100.0, left: 45.0, right: 45.0, bottom: 30.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  CircleAvatar(
-                    child: Icon(
-                      Icons.list,
-                      size: 40.0,
-                      color: kBacgroundColor,
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: EdgeInsets.only(
+                top: 100.0, left: 45.0, right: 45.0, bottom: 30.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CircleAvatar(
+                  child: Icon(
+                    Icons.list,
+                    size: 40.0,
+                    color: kBacgroundColor,
+                  ),
+                  backgroundColor: Colors.white,
+                  radius: 30.0,
+                ),
+                SizedBox(
+                  height: 30.0,
+                ),
+                Text(
+                  'Todoey',
+                  style: kTitleTextStyle,
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Text(
+                  '12 task',
+                  style: kSubtitleTextStyle,
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(30)),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 40.0),
+                child: ListView(
+                  children: [
+                    ListTileWidget(
+                      text: 'Task 1',
                     ),
-                    backgroundColor: Colors.white,
-                    radius: 30.0,
-                  ),
-                  SizedBox(
-                    height: 30.0,
-                  ),
-                  Text(
-                    'Todoey',
-                    style: kTitleTextStyle,
-                  ),
-                  SizedBox(
-                    height: 10.0,
-                  ),
-                  Text(
-                    '12 task',
-                    style: kSubtitleTextStyle,
-                  ),
-                  SizedBox(
-                    height: 20.0,
-                  ),
-                ],
-              ),
-            ),
-            Expanded(
-              child: Container(
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(30),
-                      topRight: Radius.circular(30)),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 40.0),
-                  child: ListView(
-                    children: [
-                      ListTileWidget(
-                        text: 'Task 1',
-                      ),
-                      ListTileWidget(
-                        text: 'Task 2',
-                      ),
-                      ListTileWidget(
-                        text: 'Task 3',
-                      ),
-                    ],
-                  ),
+                    ListTileWidget(
+                      text: 'Task 2',
+                    ),
+                    ListTileWidget(
+                      text: 'Task 3',
+                    ),
+                  ],
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
--- todoey_flutter/lib/src/widgets/list_tile_widget.dart
+++ todoey_flutter/lib/src/widgets/list_tile_widget.dart
@@ -1,21 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:todoey_flutter/src/utils/constants.dart';
 
-class ListTileWidget extends StatelessWidget {
+class ListTileWidget extends StatefulWidget {
   final String text;
   ListTileWidget({this.text});
+
+  @override
+  _ListTileWidgetState createState() => _ListTileWidgetState();
+}
+
+class _ListTileWidgetState extends State<ListTileWidget> {
+  bool isChecked = false;
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
       title: Text(
-        text,
+        widget.text,
         style: kListTileTextStyle,
       ),
-      trailing: Checkbox(
-        value: false,
-        onChanged: (value) {},
-      ),
+      trailing: TaskCheckbox(),
+    );
+  }
+}
+
+class TaskCheckbox extends StatelessWidget {
+  final bool checkbosState;
+  TaskCheckbox({this.checkbosState});
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      activeColor: Color(0xFF63C9FE),
+      value: checkbosState,
+      onChanged: (newValue) {},
     );
   }
 }
