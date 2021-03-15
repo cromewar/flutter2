d6184e7bd3a33eb3ad1142ecd8caea55ec3eb083
cromewar <vasiliy@cromewar.com>
2021-03-15 10:37:02 -0500

    creating the interface for maps page

--- qr_scanner/lib/pages/maps_page.dart
+++ qr_scanner/lib/pages/maps_page.dart
@@ -5,8 +5,21 @@ class MapHistory extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Maps Page'),
+    return ListView.builder(
+      itemBuilder: (_, i) => ListTile(
+        leading: Icon(
+          Icons.map,
+          color: Theme.of(context).primaryColor,
+        ),
+        title: Text('http://sample.com'),
+        subtitle: Text('ID: 1'),
+        trailing: Icon(
+          Icons.keyboard_arrow_right,
+          color: Colors.grey,
+        ),
+        onTap: () {},
+      ),
+      itemCount: 10,
     );
   }
 }
